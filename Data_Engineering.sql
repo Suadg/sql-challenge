c999fd9c44c7f655f11a5ceb0e0732756a0554bb
Suad G <162393107+Suadg@users.noreply.github.com>
2024-05-02 23:00:34 -0400

    Update Data_Engineering.sql

--- Data_Engineering.sql
+++ Data_Engineering.sql
@@ -1,8 +1,9 @@
+# Query 
+
 CREATE TABLE "departments" (
-    "dept_no" VARCHAR(10)   NOT NULL,
-    "dept_name" VARCHAR(30)   NOT NULL,
-	)
-	;
+    dept_no VARCHAR(10) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE "dept_emp" (
     "emp_no" INTEGER   NOT NULL,
@@ -15,7 +16,7 @@ CREATE TABLE "dept_manager" (
 );
 
 CREATE TABLE "employees" (
-    "emp_no" INTEGER   NOT NULL,
+    "emp_no" INTEGER PRIMARY KEY  NOT NULL,
     "emp_title_id" VARCHAR(10)   NOT NULL,
     "birth_date" VARCHAR  NOT NULL,
     "first_name" VARCHAR(30)   NOT NULL,
